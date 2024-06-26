31b4f3c81451cdd678afda89db32e0d75f499512
Jean Paul Fernandez <jpxoi@icloud.com>
2024-06-25 21:12:21 -0500

    chore: Update pastoralID component error handling and redirection

--- app/ui/components/pastoralID.tsx
+++ app/ui/components/pastoralID.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState, useRef } from "react";
 export default function PastoralID() {
   const router = useRouter()
   const [userID, setUserID] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const [countdown, setCountdown] = useState<string | null>(null);
   const imageRef = useRef<HTMLImageElement>(null);
 
   const imageLoader = ({ src } : { src: string }) => {
@@ -27,13 +27,14 @@ export default function PastoralID() {
   }, [router]);
 
   const handleImageError = () => {
-    setError("No se pudo cargar tu Pastoral ID. Serás redirigido a la página principal en 5 segundos.");
     hideImage();
+    setCountdown("5");
 
     setTimeout(() => {
       setUserID(null);
       localStorage.removeItem("id");
       router.push('/');
+      setCountdown(null);
     }, 5000);
   }
 
@@ -43,16 +44,15 @@ export default function PastoralID() {
     }
   }
 
-
   return (
     <div
       id="pastoral_id"
       className="pass-front w-full max-w-xs sm:max-w-sm h-auto m-0 p-0 rounded-lg transition-all duration-300"
     >
       {
-        error && (
+        countdown && (
           <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
-            {error}
+            No se pudo cargar tu Pastoral ID. Serás redirigido a la página principal en {countdown} segundos.
           </div>
         )
       }
