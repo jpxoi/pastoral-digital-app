0f8c48378a450d7bc1feb8793da55cc5c31afbe7
Jean Paul Fernandez <jpxoi@icloud.com>
2024-06-26 18:19:22 -0500

    chore: Update page title and description in layout component

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard | Pastoral Digital Services",
+  description:
+    "Pastoral Digital es una plataforma digital para uso interno de la Pastoral Mariana del CEP Nuestra Señora del Perpetuo Socorro",
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+    {children}
+    </>
+  );
+}
