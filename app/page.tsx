c0cc6f85b63deace1a3e622d3c2946bc3416b562
Jean Paul Fernandez <jpxoi@icloud.com>
2024-06-26 20:32:23 -0500

    Remove PWAPrompt component and its dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Footer from "./ui/components/footer";
 import FormContainer from "./ui/components/formContainer";
-import PWAPrompt from "./ui/components/pwaPrompt";
 
 export default function Home() {
   return (
     <main className="flex min-h-[100dvh] w-full flex-col items-center justify-center">
       <FormContainer />
-      <PWAPrompt />
       <Footer />
     </main>
   );
