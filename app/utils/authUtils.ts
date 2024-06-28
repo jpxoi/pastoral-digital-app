ffdc89bb31269229df2055f28f9eda142360b2e7
Jean Paul Fernandez <jpxoi@icloud.com>
2024-06-28 13:12:20 -0500

    refactor: Update authUtils.ts and localStorageUtils.ts for user authentication and session management

---
+++ app/utils/authUtils.ts
@@ -0,0 +1,32 @@
+import { clearLocalStorage, getLocalStorageItems } from "./localStorageUtils";
+
+export function checkExpiryDate(expiryDate: string): boolean {
+  return new Date(expiryDate) > new Date();
+}
+
+export function calculateExpiryDate(): string {
+  const date = new Date();
+  date.setDate(date.getDate() + 1);
+  return date.toISOString();
+}
+
+export function checkUserLoggedIn(): boolean {
+  const {
+    id: savedID,
+    token: savedToken,
+    expiryDate,
+  } = getLocalStorageItems(["id", "token", "expiryDate"]);
+
+  const idPattern = /^[A-Za-z]{2}-\d{3}$/;
+
+  return (
+    checkExpiryDate(expiryDate) &&
+    idPattern.test(savedID) &&
+    savedID != null &&
+    savedToken != null
+  );
+}
+
+export function logOut(): void {
+  clearLocalStorage();
+}
--- app/utils/localStorageUtils.ts
+++ app/utils/localStorageUtils.ts
@@ -2,6 +2,18 @@ export const getLocalStorageItem = (key: string): string | null => {
   return localStorage.getItem(key);
 };
 
+export const getLocalStorageItems = (
+  keys: string[]
+): Record<string, string> => {
+  const items: Record<string, string> = {};
+
+  keys.forEach((key) => {
+    items[key] = localStorage.getItem(key) as string;
+  });
+
+  return items;
+};
+
 export const setLocalStorageItem = (key: string, value: string): void => {
   localStorage.setItem(key, value);
 };
@@ -10,7 +22,11 @@ export const setLocalStorageItems = (items: Record<string, string>): void => {
   Object.entries(items).forEach(([key, value]) => {
     localStorage.setItem(key, value);
   });
-}
+};
+
+export const removeLocalStorageItem = (key: string): void => {
+  localStorage.removeItem(key);
+};
 
 export const clearLocalStorage = (): void => {
   localStorage.clear();
