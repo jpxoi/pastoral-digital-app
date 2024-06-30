04ce167d9d35f0d0cc8c6dcebcdceea70e0796dd
Jean Paul Fernandez <jpxoi@icloud.com>
2024-06-30 00:41:21 -0500

    refactor: Update app manifest to use JavaScript file extension

--- app/manifest.js
+++ app/manifest.js
@@ -1,6 +1,4 @@
-import { MetadataRoute } from "next";
-
-export default function manifest(): MetadataRoute.Manifest {
+export default function manifest() {
   return {
     name: "Pastoral Digital Services",
     short_name: "Pastoral Digital",
