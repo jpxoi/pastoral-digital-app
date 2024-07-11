ed03bb75ee62a87e6690407cb4c83306e38a7cce
Jean Paul Fernandez <jpxoi@icloud.com>
2024-07-10 22:37:16 -0500

    refactor: Update attendanceTable component styles

--- app/ui/components/attendanceTable.tsx
+++ app/ui/components/attendanceTable.tsx
@@ -59,7 +59,7 @@ export default function AttendanceTable() {
 
   return (
     <>
-      <div className="flex flex-row items-center justify-center lg:justify-end gap-4 mb-2 w-full sm:max-w-screen-sm md:max-w-screen-md">
+      <div className="flex flex-row items-center justify-center lg:justify-end gap-4 mb-2 w-full">
         <button
           onClick={refreshTable}
           className="text-sm text-blue-500 hover:text-blue-700 disabled:text-gray-300 disabled:hover:text-gray-300 disabled:cursor-not-allowed flex flex-row items-center gap-1"
