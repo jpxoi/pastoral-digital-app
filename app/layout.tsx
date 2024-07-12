10f953b7c9c54cb57ea95a1dca3b031c2bfb4779
Jean Paul Fernandez <jpxoi@icloud.com>
2024-07-11 23:20:24 -0500

    refactor: Update PWA startup images and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,47 @@ export const metadata: Metadata = {
   title: "Pastoral Digital Services | Pastoral Mariana",
   description:
     "Pastoral Digital es una plataforma digital para uso interno de la Pastoral Mariana del CEP Nuestra Señora del Perpetuo Socorro",
+  appleWebApp: {
+    title: "Pastoral Digital",
+    startupImage: [
+      {
+        url: "/images/splash/apple-launch-1242x2688.png",
+        media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      {
+        url: "/images/splash/apple-launch-828x1792.png",
+        media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      {
+        url: "/images/splash/apple-launch-1125x2436.png",
+        media: "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      {
+        url: "/images/splash/apple-launch-1242x2208.png",
+        media: "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      {
+        url: "/images/splash/apple-launch-750x1334.png",
+        media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      {
+        url: "/images/splash/apple-launch-2048x2732.png",
+        media: "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      {
+        url: "/images/splash/apple-launch-1668x2388.png",
+        media: "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      {
+        url: "/images/splash/apple-launch-1668x2224.png",
+        media: "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      {
+        url: "/images/splash/apple-launch-1536x2048.png",
+        media: "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)",
+      }
+    ]
+  }
 };
 
 export const viewport: Viewport = {
@@ -24,65 +65,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        {/* PWA */}
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-
-        <link
-          rel="apple-touch-startup-image"
-          href="/images/splash/apple-launch-1242x2688.png"
-        />
-
-        {/*
-        <link
-          rel="apple-touch-startup-image"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
-          href="/images/splash/apple-launch-1242x2688.png"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
-          href="/apple-launch-828x1792.png"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-          href="/images/splash/apple-launch-1125x2436.png"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
-          href="/images/splash/apple-launch-1242x2208.png"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-          href="/images/splash/apple-launch-750x1334.png"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
-          href="/images/splash/apple-launch-2048x2732.png"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
-          href="/images/splash/apple-launch-1668x2388.png"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
-          href="/images/splash/apple-launch-1668x2224.png"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
-          href="/images/splash/apple-launch-1536x2048.png"
-        />
-        */}
-      </head>
+    <html lang="es">
       <body className={poppins.className}>{children}</body>
     </html>
   );
