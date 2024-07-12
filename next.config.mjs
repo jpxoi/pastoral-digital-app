154addd19050f427c7d6ef9731b5742610ce5aa6
Jean Paul Fernandez <jpxoi@icloud.com>
2024-07-11 22:37:24 -0500

    refactor: Remove unused cacheOnFrontEndNav option in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
