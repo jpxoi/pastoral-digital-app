c8159d37ad96e517f27381b2aed517035a215572
Jean Paul Fernandez <jpxoi@icloud.com>
2024-07-17 22:53:12 -0500

    refactor: Add interfaces for user info, attendance context, table row, status label, and user menu context

---
+++ types/interfaces.ts
@@ -0,0 +1,35 @@
+import { Session } from "@auth0/nextjs-auth0";
+import { Dispatch, SetStateAction } from "react";
+
+export interface UserInfoProps {
+  avatarURL: string | null;
+  fallbackAvatar: string | null;
+  userID: string | null;
+  userToken: string | null;
+  userCustomAvatar: boolean;
+}
+
+export interface AttendanceContextProps {
+  data: never[];
+  loading: boolean;
+  error: string | null;
+  refreshButtonText: string;
+  refreshTable: () => void;
+}
+
+export interface TableRow {
+  row: {
+    [key: string]: string;
+  };
+}
+
+export interface StatusLabel {
+  status: string;
+}
+
+export interface UserMenuContextProps {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  userSession: Session["user"];
+  userInfo: UserInfoProps;
+}
