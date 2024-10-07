ef68761279a24f86f9784bfb4c80c99b0a719d41
Jean Paul Fernandez <jpxoi@icloud.com>
2024-10-07 13:12:58 +0100

    refactor: Update home page to use ClosingAppScreen component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { FooterNarrow } from '@/components/shared/footer'
-import WelcomeScreen from '@/components/home/welcomeScreen'
+import ClosingAppScreen from '@/components/home/closingAppScreen'
 import Background from '@/components/shared/background'
 
 export default async function Home() {
   return (
     <main className='flex h-dvh w-full flex-col items-center justify-center bg-center'>
       <Background />
-      <WelcomeScreen />
+      <ClosingAppScreen />
       <footer className='mt-4 hidden sm:block'>
         <FooterNarrow />
       </footer>
