4466daaad849f486a41340e0e2799df8c96f8d48
Jean Paul Fernandez <jpxoi@icloud.com>
2024-11-23 23:22:44 +0000

    feat: Add sign-up page with Clerk integration and loading states

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,17 @@
+import Loading from '@/app/loading'
+import Background from '@/components/shared/background'
+import { ClerkLoaded, ClerkLoading, SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+    <main className='flex h-dvh w-full flex-col items-center justify-center bg-center'>
+      <Background />
+      <ClerkLoading>
+        <Loading />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignUp fallbackRedirectUrl="/dashboard" />
+      </ClerkLoaded>
+    </main>
+  )
+}
