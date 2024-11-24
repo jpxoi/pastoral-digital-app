a82701b5a6033405d326aac50425db792160381b
Jean Paul Fernandez <jpxoi@icloud.com>
2024-11-24 01:49:58 +0000

    feat: Update PastoralID component to display QR code for identity verification

--- components/dashboard/pastoralID.tsx
+++ components/dashboard/pastoralID.tsx
@@ -3,30 +3,30 @@ import { auth, currentUser } from '@clerk/nextjs/server'
 import { UserInfoProps } from '@/types/interfaces'
 import { fetchUserInfoByEmail } from '@/utils/fetchUtils'
 import PastoralIDDownloader from '@/components/dashboard/pastoralIDDownloader'
+import Background from '../shared/background'
 
 export default async function PastoralID() {
   const user = await currentUser()
-  const userInfo: UserInfoProps = await fetchUserInfoByEmail({
-    email: user?.primaryEmailAddress?.emailAddress as string,
-  })
 
   return (
     <div className='pass-front group m-0 h-auto min-w-80 rounded-lg p-0 transition-all duration-300 sm:min-w-96 sm:max-w-sm'>
-      <div className='group relative flex cursor-pointer items-center justify-center overflow-hidden rounded-lg bg-white drop-shadow-md transition-all duration-300 hover:drop-shadow-2xl'>
-        <Image
-          id='pastoral_id_img'
-          className='m-0 mx-auto h-auto w-full max-w-xs rounded-lg p-0 sm:max-w-sm'
-          src={`${process.env.CDN_URL}/media/pastoralid/${userInfo.userID}.png`}
-          placeholder='blur'
-          blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vz1UwAJDgOebYQBlwAAAABJRU5ErkJggg=='
-          alt='Pastoral ID'
-          width='450'
-          height='575'
-        />
+      <div className='group relative flex cursor-pointer items-center justify-center overflow-hidden rounded-lg border-2 border-[#001944] bg-white drop-shadow-md transition-all duration-300 hover:drop-shadow-2xl'>
+        <div className='relative h-full w-full'>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={`https://quickchart.io/qr?text=${user?.id}&margin=2&size=300&format=svg&dark=001944`}
+            className='m-0 mx-auto h-auto w-full max-w-xs rounded-lg p-0 sm:max-w-sm'
+            width={300}
+            height={300}
+            alt='Pastoral ID QR Code'
+          />
+          <div className='flex h-full flex-col items-center justify-center bg-[#001944] p-2'>
+            <p className='text-balance text-xs text-blue-50'>
+              Escanea este QR para acceder verificar tu identidad
+            </p>
+          </div>
+        </div>
         <div className='z-5 absolute -inset-full top-0 block h-full w-1/2 -skew-x-12 transform bg-gradient-to-r from-transparent to-white opacity-40 group-hover:animate-shine' />
-        <PastoralIDDownloader
-          url={`${process.env.CDN_URL}/media/pastoralid/${userInfo.userID}.png`}
-        />
       </div>
     </div>
   )
