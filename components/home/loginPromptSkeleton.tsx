e6ab3150966b67f89859d082e47e2ef3510f79f5
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-14 14:49:57 +0000

    feat: Add additional loading placeholder to LoginPromptSkeleton component

--- components/home/loginPromptSkeleton.tsx
+++ components/home/loginPromptSkeleton.tsx
@@ -2,6 +2,7 @@ export default function LoginPromptSkeleton() {
   return (
     <div id='form-sekeleton' className='mt-6 flex flex-col items-center gap-4'>
       <div className='h-[3.125rem] w-full animate-pulse rounded-md bg-gray-200'></div>
+      <div className='h-[3.125rem] w-full animate-pulse rounded-md bg-gray-200'></div>
     </div>
   )
 }
