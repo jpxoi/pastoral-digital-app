cff8c7ed339f34e064cec4d075d8b5c3d9081f09
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-14 14:49:51 +0000

    feat: Refactor ProfilePicSettings to use currentUser directly and remove unused fetchUserInfoByEmail

--- components/settings/ProfilePicSettings.tsx
+++ components/settings/ProfilePicSettings.tsx
@@ -1,18 +1,13 @@
 import ChangeProfilePic from '@/components/settings/changeProfilePic'
-import { UserInfoProps } from '@/types/interfaces'
-import { fetchUserInfoByEmail } from '@/utils/fetchUtils'
-import { auth, currentUser } from '@clerk/nextjs/server'
+import { currentUser } from '@clerk/nextjs/server'
 
 export default async function ProfilePicSettings() {
   const user = await currentUser()
-  const userInfo: UserInfoProps = await fetchUserInfoByEmail({
-    email: user?.primaryEmailAddress?.emailAddress as string,
-  })
   return (
     <div className='flex w-full flex-col items-start gap-6 rounded-xl bg-white p-8 shadow-md'>
       <h5 className='text-xl font-medium text-gray-800'>Foto de Perfil</h5>
       <ChangeProfilePic
-        userID={userInfo.userID as string}
+        userID={user?.id as string}
         userFullName={user?.fullName as string}
         avatarURL={user?.imageUrl as string}
       />
