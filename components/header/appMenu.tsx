6f0d31cdbb24ba8e5b882e51e129acba077947d6
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-20 16:26:41 +0000

    feat: Update AppMenu component to display admin links based on user role

--- components/header/appMenu.tsx
+++ components/header/appMenu.tsx
@@ -1,11 +1,34 @@
-import { MenuIcon } from '@/components/icons/icons24'
+import { getUserById } from '@/queries/select'
+import { currentUser } from '@clerk/nextjs/server'
+import { ListCheckIcon, ListOrderedIcon, QrCodeIcon } from 'lucide-react'
+import Link from 'next/link'
 
-export default function AppMenu() {
+export default async function AppMenu() {
+  const user = await currentUser()
+  const userDb = await getUserById(user?.id || '')
+  const { role } = userDb[0]
+
   return (
-    <div className='flex items-center justify-center'>
-      <button className='flex cursor-not-allowed items-center gap-2 text-white/50'>
-        <MenuIcon />
-      </button>
-    </div>
+    <>
+      {role === 'admin' ? (
+        <div className='flex items-center justify-center gap-3'>
+          <Link href='/admin/records' className='flex items-center gap-2 text-white'>
+            <ListCheckIcon />
+          </Link>
+          <Link href='/admin/scan' className='flex items-center gap-2 text-white'>
+            <QrCodeIcon />
+          </Link>
+        </div>
+      ) : (
+        <div className='flex items-center justify-center'>
+          <Link
+            href='#'
+            className='flex cursor-not-allowed items-center gap-2 text-white/50'
+          >
+            <QrCodeIcon />
+          </Link>
+        </div>
+      )}
+    </>
   )
 }
