5e25980eae9f3ee2eacaad64bdab586c2f7c3c8c
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-20 16:27:17 +0000

    feat: Add attendance records page with loading skeleton and table display

---
+++ app/(main)/admin/records/page.tsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from '@/components/ui/card'
+import AttendanceTable from '@/components/admin/attendanceTable'
+import { Suspense } from 'react'
+import AttendanceTableSkeleton from '@/components/admin/attendanceTableSkeleton'
+
+export default function Page() {
+  return (
+    <main className='container mx-auto p-4 md:p-8'>
+      <Card>
+        <CardHeader>
+          <CardTitle>Registro de Asistencia</CardTitle>
+          <CardDescription>
+            Asistencias registradas en las últimas 24 horas
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Suspense fallback={<AttendanceTableSkeleton />}>
+            <AttendanceTable />
+          </Suspense>
+        </CardContent>
+        {/* <CardFooter className='flex justify-end'>
+        </CardFooter> */}
+      </Card>
+    </main>
+  )
+}
