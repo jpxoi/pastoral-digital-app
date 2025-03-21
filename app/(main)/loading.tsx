80e59e7b7221fdd4e4403e5c7289fc55f964a16c
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-21 08:42:03 +0000

    feat: Refactor Loading component layout for improved styling and structure

--- app/(main)/loading.tsx
+++ app/(main)/loading.tsx
@@ -2,8 +2,8 @@ import Spinner from '@/components/shared/spinner'
 
 export default function Loading() {
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center'>
+    <main className='flex h-screen w-full flex-1 flex-col items-center justify-center rounded-tl-2xl border border-neutral-200 bg-white p-2 md:p-10'>
       <Spinner />
-    </div>
+    </main>
   )
 }
