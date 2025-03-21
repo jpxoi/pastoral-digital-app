4d6bec501873e2faf10bc45524968d8af696e994
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-21 14:57:11 +0000

    refactor: Simplify layout structure and improve accessibility in attendance records page

--- app/(main)/admin/records/page.tsx
+++ app/(main)/admin/records/page.tsx
@@ -1,32 +1,19 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
 import AttendanceTable from '@/components/admin/attendanceTable'
 import { Suspense } from 'react'
 import AttendanceTableSkeleton from '@/components/admin/attendanceTableSkeleton'
 
 export default async function Page() {
   return (
-    <main className='flex h-full w-full flex-1 flex-col gap-2 rounded-tl-2xl border border-neutral-200 bg-white p-2 md:p-10'>
-      <Card>
-        <CardHeader>
-          <CardTitle>Registro de Asistencia</CardTitle>
-          <CardDescription>
-            Asistencias registradas en las últimas 24 horas
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Suspense fallback={<AttendanceTableSkeleton />}>
-            <AttendanceTable />
-          </Suspense>
-        </CardContent>
-        {/* <CardFooter className='flex justify-end'>
-        </CardFooter> */}
-      </Card>
+    <main className='flex h-full w-full flex-1 flex-col gap-4 rounded-tl-2xl border border-neutral-200 bg-white p-4 md:p-10'>
+      <div className='flex flex-col gap-2'>
+        <h1 className='text-xl sm:text-2xl font-semibold'>Registro de Asistencia</h1>
+        <p className='text-sm text-neutral-500'>
+          Aquí puedes ver la lista de asistencia de los usuarios.
+        </p>
+      </div>
+      <Suspense fallback={<AttendanceTableSkeleton />}>
+        <AttendanceTable />
+      </Suspense>
     </main>
   )
 }
