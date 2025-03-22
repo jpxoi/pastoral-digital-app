1ee7b2c7d147f262892aeb779cbd76e293ebc4f9
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-22 23:26:29 +0000

    feat: Create Birthday page with grid layout and offline alert

---
+++ app/(main)/birthdays/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from 'next'
+import OfflineAlert from '@/components/shared/offlineAlert'
+import BirthdayGrid from '@/components/birthdays/birthdayGrid'
+import BirthdayGridSkeleton from '@/components/birthdays/birthdayGridSkeleton'
+import { Suspense } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Cumpleaños | Pastoral Digital App',
+}
+
+export default function Page() {
+  return (
+    <main className='flex h-full w-full flex-1 flex-col gap-4 overflow-y-scroll rounded-tl-2xl border border-neutral-200 bg-white p-4 max-sm:max-h-[calc(100vh-3rem)] md:p-10'>
+      <OfflineAlert />
+      <div className='flex flex-col gap-2'>
+        <h1 className='text-xl font-semibold sm:text-2xl'>
+          Próximos Cumpleaños
+        </h1>
+        <p className='text-sm text-neutral-500'>
+          Aquí puedes ver los cumpleaños de los catequistas en los próximos
+          días.
+        </p>
+      </div>
+      <Suspense fallback={<BirthdayGridSkeleton />}>
+        <BirthdayGrid />
+      </Suspense>
+    </main>
+  )
+}
+
+export const dynamic = 'force-dynamic'
