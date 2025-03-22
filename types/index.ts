0b550b00eddf486eb4d840d1fcd10196cc8f8e0d
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-22 10:54:35 +0000

    feat: Add TypeScript interfaces and enums for user and attendance management

--- types/index.ts
+++ types/index.ts
@@ -29,3 +29,22 @@ export interface StatusLabel {
 export interface FetchAttendanceProps extends SelectAttendance {
   user: SelectUser
 }
+
+export enum AttendanceStatus {
+  A_TIEMPO = 'A TIEMPO',
+  TARDANZA = 'TARDANZA',
+  DOBLE_TARDANZA = 'DOBLE TARDANZA',
+  FALTA_JUSTIFICADA = 'FALTA JUSTIFICADA',
+  TARDANZA_JUSTIFICADA = 'TARDANZA JUSTIFICADA',
+  FALTA_INJUSTIFICADA = 'FALTA INJUSTIFICADA',
+}
+
+export enum UserRole {
+  MEMBER = 'member',
+  ADMIN = 'admin',
+}
+
+export enum UserCategory {
+  STUDENT = 'student',
+  ALUMNI = 'alumni',
+}
