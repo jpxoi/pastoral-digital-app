8c3020d5746bdc290d055a34df392b9aa677417d
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-23 11:13:47 +0000

    feat: Implement user onboarding completion process and update user metadata

--- actions/user.ts
+++ actions/user.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { auth, clerkClient } from '@clerk/nextjs/server'
 import { createUser } from '@/queries/insert'
 import { z } from 'zod'
 import { OnboardingFormSchema } from '@/schema'
@@ -17,11 +18,47 @@ export const registerUser = async (
   }
 
   return await createUser(validatedFields.data)
-    .then(() => {
-      return { success: 'Usuario registrado exitosamente' }
+    .then(async () => {
+      const onboardingResult = await completeOnboarding()
+
+      if (onboardingResult.error) {
+        throw new Error(onboardingResult.error)
+      }
+
+      if (onboardingResult.success) {
+        return { success: 'Usuario registrado exitosamente' }
+      }
+
+      return { error: 'Error al completar el registro del usuario' }
     })
     .catch((error) => {
       console.error(error)
       return { error: 'Error al registrar el usuario. Intenta nuevamente.' }
     })
+}
+
+export const completeOnboarding = async () => {
+  const { userId } = await auth()
+
+  if (!userId) {
+    return { error: 'No se pudo obtener el ID del usuario.' }
+  }
+
+  const currentUserRole = (await auth()).sessionClaims?.metadata.role
+
+  const client = await clerkClient()
+
+  try {
+    const res = await client.users.updateUser(userId, {
+      publicMetadata: {
+        role: currentUserRole,
+        onboardingComplete: true,
+      },
+    })
+
+    return { success: 'Metadata del usuario actualizada exitosamente.' }
+  } catch (err) {
+    console.error(err)
+    return { error: 'Hubo un error al actualizar los metadatos del usuario.' }
+  }
 }
