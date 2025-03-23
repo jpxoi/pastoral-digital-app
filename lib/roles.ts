498a1e0169c86bda75b9450703d606e237905803
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-23 13:27:50 +0000

    refactor: Update role type from Roles to UserRole in checkRole function

--- lib/roles.ts
+++ lib/roles.ts
@@ -1,9 +1,9 @@
 'use server'
 
-import { Roles } from '@/types/globals'
+import { UserRole } from '@/types'
 import { auth } from '@clerk/nextjs/server'
 
-export const checkRole = async (role: Roles) => {
+export const checkRole = async (role: UserRole) => {
   const { sessionClaims } = await auth()
   return sessionClaims?.metadata.role === role
 }
