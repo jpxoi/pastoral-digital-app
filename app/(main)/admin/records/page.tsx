c08cc96c70722b852dbaa007b6468b1319fd04d9
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-23 00:27:19 +0000

    feat: Update page title and adjust text alignment in admin records page

--- app/(main)/admin/records/page.tsx
+++ app/(main)/admin/records/page.tsx
@@ -5,14 +5,14 @@ import { Metadata } from 'next'
 import OfflineAlert from '@/components/shared/offlineAlert'
 
 export const metadata: Metadata = {
-  title: 'Registros de Asistencia | Pastoral Digital App',
+  title: 'Registro de Asistencias | Pastoral Digital App',
 }
 
 export default async function Page() {
   return (
     <main className='flex h-full w-full flex-1 flex-col gap-4 overflow-y-scroll rounded-tl-2xl border border-neutral-200 bg-white p-4 max-sm:max-h-[calc(100vh-3rem)] md:p-10'>
       <OfflineAlert />
-      <div className='flex flex-col gap-2'>
+      <div className='flex flex-col gap-2 text-left'>
         <h1 className='text-xl font-semibold sm:text-2xl'>
           Registro de Asistencia
         </h1>
