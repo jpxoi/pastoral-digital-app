19a4bcc47511b893d8d791f5b9ed1809c10cb810
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-24 23:42:38 +0000

    feat: Remove RedirectToDashboard component as it is no longer needed

--- components/dashboard/redirectToDashboard.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
-
-export default function RedirectToDashboard() {
-  const router = useRouter()
-  router.push('/dashboard')
-  return null
-}
