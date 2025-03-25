a367109c557ece9d4f800d57242a7abebb6ed45d
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-25 23:10:46 +0000

    feat: Increase default count in EventCardGroupSkeleton and update layout structure

--- components/events/eventCardGroupSkeleton.tsx
+++ components/events/eventCardGroupSkeleton.tsx
@@ -1,15 +1,15 @@
 import EventCardSkeleton from './eventCardSkeleton'
 
 export default function EventCardGroupSkeleton({
-  count = 4,
+  count = 6,
 }: {
   count?: number
 }) {
   return (
-    <>
+    <div className='flex flex-col gap-2 lg:grid lg:grid-cols-2 xl:grid-cols-3'>
       {Array.from({ length: count }).map((_, i) => (
         <EventCardSkeleton key={i} />
       ))}
-    </>
+    </div>
   )
 }
