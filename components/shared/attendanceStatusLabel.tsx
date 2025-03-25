bbca5d88376f3f0a2380738d6986b8ba82e0db73
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-25 17:26:00 +0000

    feat: Update attendance status labels to prevent text wrapping

--- components/shared/attendanceStatusLabel.tsx
+++ components/shared/attendanceStatusLabel.tsx
@@ -14,37 +14,37 @@ export default function AttendanceStatusLabel({ status }: StatusLabel) {
     'A TIEMPO': (
       <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-green-50 p-2 text-xs text-green-700'>
         <IconClockCheck className='size-4' />
-        <span>A TIEMPO</span>
+        <span className='text-nowrap'>A TIEMPO</span>
       </span>
     ),
     TARDANZA: (
       <span className='flex max-w-fit flex-nowrap justify-start gap-1 rounded-lg bg-yellow-50 p-2 text-xs text-yellow-700'>
         <IconClockExclamation className='size-4' />
-        <span>TARDANZA</span>
+        <span className='text-nowrap'>TARDANZA</span>
       </span>
     ),
     'DOBLE TARDANZA': (
       <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-orange-50 p-2 text-xs text-orange-700'>
         <IconClockX className='size-4' />
-        <span>DOBLE TARDANZA</span>
+        <span className='text-nowrap'>DOBLE TARDANZA</span>
       </span>
     ),
     'FALTA INJUSTIFICADA': (
       <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-red-50 p-2 text-xs text-red-700'>
         <IconCalendarX className='size-4' />
-        <span>FALTA INJUSTIFICADA</span>
+        <span className='text-nowrap'>FALTA INJUSTIFICADA</span>
       </span>
     ),
     'TARDANZA JUSTIFICADA': (
       <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-purple-50 p-2 text-xs text-purple-700'>
         <IconClockHeart className='size-4' />
-        <span>TARDANZA JUSTIFICADA</span>
+        <span className='text-nowrap'>TARDANZA JUSTIFICADA</span>
       </span>
     ),
     'FALTA JUSTIFICADA': (
       <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-blue-50 p-2 text-xs text-blue-700'>
         <IconCalendarHeart className='size-4' />
-        <span>FALTA JUSTIFICADA</span>
+        <span className='text-nowrap'>FALTA JUSTIFICADA</span>
       </span>
     ),
   }
