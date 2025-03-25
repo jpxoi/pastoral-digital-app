aacb8a2e923e6674e42071a170684e6fae40dfb0
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-25 12:17:55 +0000

    feat: Improve layout and sizing of Skeleton components in EventCardSkeleton

--- components/events/eventCardSkeleton.tsx
+++ components/events/eventCardSkeleton.tsx
@@ -12,8 +12,9 @@ export default function EventCardSkeleton() {
   return (
     <Card className='text-left'>
       <CardHeader>
-        <CardTitle className='text-lg'>
+        <CardTitle className='flex items-center justify-between'>
           <Skeleton className='h-7 w-3/4' />
+          <Skeleton className='size-6' />
         </CardTitle>
         <CardDescription className='space-y-0.5'>
           <span className='flex items-center gap-1 text-ellipsis'>
@@ -22,7 +23,7 @@ export default function EventCardSkeleton() {
           </span>
           <span className='flex items-center gap-1 text-ellipsis'>
             <Skeleton className='size-4' />
-            <Skeleton className='h-5 w-16' />
+            <Skeleton className='h-5 w-36' />
           </span>
           <span className='flex items-center gap-1 text-ellipsis text-primary hover:underline'>
             <Skeleton className='size-4' />
@@ -31,8 +32,8 @@ export default function EventCardSkeleton() {
         </CardDescription>
       </CardHeader>
       <CardContent className='flex items-center justify-between gap-2'>
-        <Skeleton className='h-5 w-1/4' />
-        <Skeleton className='my-0.5 h-5 w-1/4' />
+        <Skeleton className='h-6 w-40' />
+        <Skeleton className='h-6 w-28' />
       </CardContent>
     </Card>
   )
