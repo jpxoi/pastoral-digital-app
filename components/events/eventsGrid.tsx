fec43ca3c583ed1898c413b09a9ec9248cf94e0c
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-25 23:10:41 +0000

    feat: Implement tabbed navigation for events grid to enhance user experience

--- components/events/eventsGrid.tsx
+++ components/events/eventsGrid.tsx
@@ -2,28 +2,25 @@ import { Suspense } from 'react'
 import PastEvents from './pastEvents'
 import UpcomingEvents from './upcomingEvents'
 import EventCardGroupSkeleton from './eventCardGroupSkeleton'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs'
 
 export default function EventsGrid() {
   return (
-    <div className='grid gap-4 text-left md:grid-cols-2 xl:grid-cols-3'>
-      <div className='flex flex-col gap-2 xl:col-span-2'>
-        <div className='grid gap-2 xl:grid-cols-2'>
-          <h2 className='col-span-full text-lg font-medium'>
-            Próximos eventos
-          </h2>
-          <Suspense fallback={<EventCardGroupSkeleton count={6} />}>
-            <UpcomingEvents />
-          </Suspense>
-        </div>
-      </div>
-      <div className='flex flex-col gap-2'>
-        <div className='grid gap-2'>
-          <h2 className='col-span-full text-lg font-medium'>Eventos pasados</h2>
-          <Suspense fallback={<EventCardGroupSkeleton count={3} />}>
-            <PastEvents />
-          </Suspense>
-        </div>
-      </div>
-    </div>
+    <Tabs defaultValue='upcoming' className='w-full'>
+      <TabsList className='mb-2 grid w-full grid-cols-2'>
+        <TabsTrigger value='upcoming'>Próximos eventos</TabsTrigger>
+        <TabsTrigger value='past'>Eventos pasados</TabsTrigger>
+      </TabsList>
+      <TabsContent value='upcoming'>
+        <Suspense fallback={<EventCardGroupSkeleton />}>
+          <UpcomingEvents />
+        </Suspense>
+      </TabsContent>
+      <TabsContent value='past'>
+        <Suspense fallback={<EventCardGroupSkeleton />}>
+          <PastEvents />
+        </Suspense>
+      </TabsContent>
+    </Tabs>
   )
 }
