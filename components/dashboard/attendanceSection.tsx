7d8d10181a5cd639b2d7286f03163124337ddd73
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-26 00:10:39 +0000

    feat: Replace Card component with DashboardCards for improved attendance display

--- components/dashboard/attendanceSection.tsx
+++ components/dashboard/attendanceSection.tsx
@@ -1,19 +1,18 @@
 import { Suspense } from 'react'
 import AttendanceTable from '@/components/dashboard/attendanceTable'
-import { Card, CardHeader } from '@/components/ui/card'
 import AttendanceTableSkeleton from './attendanceTableSkeleton'
+import DashboardCards from './dashboardCards'
+import DashboardCardsSkeleton from './dashboardCardsSkeleton'
 
 export default function AttendanceSection() {
   return (
-    <div className='container mx-auto flex h-auto max-w-[100vw] flex-col items-center justify-center'>
-      <h1 className='mb-4 text-xl font-bold sm:text-2xl'>
-        Registro de Asistencias
-      </h1>
-      <Card className='relative w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-h-[65vh]'>
-        <Suspense fallback={<AttendanceTableSkeleton />}>
-          <AttendanceTable />
-        </Suspense>
-      </Card>
+    <div className='flex h-auto w-full flex-col items-center justify-start gap-4'>
+      <Suspense fallback={<DashboardCardsSkeleton />}>
+        <DashboardCards />
+      </Suspense>
+      <Suspense fallback={<AttendanceTableSkeleton />}>
+        <AttendanceTable />
+      </Suspense>
     </div>
   )
 }
