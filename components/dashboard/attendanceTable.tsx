b37fb61625944e29973da3639201cfce6f021c19
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-26 21:35:40 +0000

    feat: Adjust layout of attendance table for full-width display

--- components/dashboard/attendanceTable.tsx
+++ components/dashboard/attendanceTable.tsx
@@ -10,7 +10,7 @@ export default async function AttendanceTable() {
   )
 
   return (
-    <div className='flex flex-col gap-2'>
+    <div className='flex w-full flex-col gap-2'>
       <h1 className='text-left text-lg font-bold'>Mis Asistencias</h1>
       <SimpleDataTable
         columns={UserAttendanceColumns}
