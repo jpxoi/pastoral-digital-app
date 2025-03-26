77f5f44be2befd1503df221ae8a43ccdbd6d88d5
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-26 00:10:58 +0000

    feat: Update loading component to use DashboardCardsSkeleton for improved loading experience

--- app/(main)/dashboard/loading.tsx
+++ app/(main)/dashboard/loading.tsx
@@ -1,11 +1,12 @@
 import AttendanceTableSkeleton from '@/components/dashboard/attendanceTableSkeleton'
-import { Card } from '@/components/ui/card'
+import DashboardCardsSkeleton from '@/components/dashboard/dashboardCardsSkeleton'
 import { Skeleton } from '@/components/ui/skeleton'
 
 export default function Loading() {
   return (
     <main className='flex h-full w-full flex-1 flex-col gap-4 overflow-y-scroll rounded-tl-2xl border border-neutral-200 bg-white p-4 pb-8 max-sm:max-h-[calc(100vh-3rem)] md:p-8'>
-      <div className='flex w-full flex-col items-center justify-start lg:flex-row lg:items-start lg:justify-evenly'>
+      <div className='flex w-full flex-col items-center justify-start gap-4 md:flex-row md:items-start md:justify-between'>
+        {/* Pastoral ID */}
         <div className='pass-front group m-0 h-auto w-full min-w-80 rounded-lg p-0 transition-all duration-300 sm:min-w-96 sm:max-w-sm'>
           <div className='group relative flex cursor-pointer items-center justify-center overflow-hidden rounded-lg border-2 border-[#001944] bg-white drop-shadow-md transition-all duration-300 hover:drop-shadow-2xl'>
             <div className='relative h-full w-full'>
@@ -19,15 +20,11 @@ export default function Loading() {
             <div className='z-5 absolute -inset-full top-0 block h-full w-1/2 -skew-x-12 transform bg-gradient-to-r from-transparent to-white opacity-40 group-hover:animate-shine' />
           </div>
         </div>
-        <div className='mt-8 flex flex-col items-center justify-start gap-4 lg:ml-4 lg:mt-0 lg:max-h-[80vh] xl:ml-0'>
-          <div className='container mx-auto flex h-auto max-w-[100vw] flex-col items-center justify-center'>
-            <h1 className='mb-4 text-xl font-bold sm:text-2xl'>
-              Registro de Asistencias
-            </h1>
-            <Card className='relative w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-h-[65vh]'>
-              <AttendanceTableSkeleton />
-            </Card>
-          </div>
+
+        {/* Attendance Section */}
+        <div className='flex h-auto w-full flex-col items-center justify-start gap-4'>
+          <DashboardCardsSkeleton />
+          <AttendanceTableSkeleton />
         </div>
       </div>
     </main>
