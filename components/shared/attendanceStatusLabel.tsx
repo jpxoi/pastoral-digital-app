ae9d1b9a38bb0c77bf547806456d624f542613e7
Jean Paul Fernandez <jpxoi@icloud.com>
2025-03-31 12:27:36 +0100

    style: Adjust spacing and rounding in AttendanceStatusLabel component for improved visual consistency

--- components/shared/attendanceStatusLabel.tsx
+++ components/shared/attendanceStatusLabel.tsx
@@ -12,37 +12,37 @@ import type { JSX } from 'react'
 export default function AttendanceStatusLabel({ status }: StatusLabel) {
   const statusMapping: { [key: string]: JSX.Element } = {
     'A TIEMPO': (
-      <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-green-50 p-2 text-xs text-green-700'>
+      <span className='flex max-w-fit flex-nowrap justify-start gap-1.5 rounded-lg bg-green-50 p-2 text-xs text-green-700'>
         <IconClockCheck className='size-4' />
         <span className='text-nowrap'>A TIEMPO</span>
       </span>
     ),
     TARDANZA: (
-      <span className='flex max-w-fit flex-nowrap justify-start gap-1 rounded-lg bg-yellow-50 p-2 text-xs text-yellow-700'>
+      <span className='flex max-w-fit flex-nowrap justify-start gap-1.5 rounded-lg bg-yellow-50 p-2 text-xs text-yellow-700'>
         <IconClockExclamation className='size-4' />
         <span className='text-nowrap'>TARDANZA</span>
       </span>
     ),
     'DOBLE TARDANZA': (
-      <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-orange-50 p-2 text-xs text-orange-700'>
+      <span className='flex max-w-fit flex-nowrap justify-start gap-1.5 rounded-lg bg-orange-50 p-2 text-xs text-orange-700'>
         <IconClockX className='size-4' />
         <span className='text-nowrap'>DOBLE TARDANZA</span>
       </span>
     ),
     'FALTA INJUSTIFICADA': (
-      <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-red-50 p-2 text-xs text-red-700'>
+      <span className='flex max-w-fit flex-nowrap items-center justify-start gap-1.5 rounded-lg bg-red-50 p-2 text-xs text-red-700'>
         <IconCalendarX className='size-4' />
         <span className='text-nowrap'>FALTA INJUSTIFICADA</span>
       </span>
     ),
     'TARDANZA JUSTIFICADA': (
-      <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-purple-50 p-2 text-xs text-purple-700'>
+      <span className='flex max-w-fit flex-nowrap justify-start gap-1.5 rounded-lg bg-purple-50 p-2 text-xs text-purple-700'>
         <IconClockHeart className='size-4' />
         <span className='text-nowrap'>TARDANZA JUSTIFICADA</span>
       </span>
     ),
     'FALTA JUSTIFICADA': (
-      <span className='flex max-w-fit flex-nowrap justify-start gap-1 bg-blue-50 p-2 text-xs text-blue-700'>
+      <span className='flex max-w-fit flex-nowrap justify-start gap-1.5 rounded-lg bg-blue-50 p-2 text-xs text-blue-700'>
         <IconCalendarHeart className='size-4' />
         <span className='text-nowrap'>FALTA JUSTIFICADA</span>
       </span>
