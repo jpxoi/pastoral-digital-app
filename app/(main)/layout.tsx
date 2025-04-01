ee14402dcad5577edf51ac93b4c6fdb24aa4b3dc
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-01 17:42:58 +0100

    refactor: Add horizontal scrolling to layout for improved content accessibility

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({
   return (
     <div className='mx-auto flex h-screen w-screen flex-1 flex-col overflow-hidden bg-gray-100 md:flex-row'>
       <CustomSidebar />
-      <div className='flex flex-1 bg-primary'>{children}</div>
+      <div className='flex flex-1 bg-primary overflow-x-scroll'>{children}</div>
     </div>
   )
 }
