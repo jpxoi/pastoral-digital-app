9d60a574fe4addf62779fae91df99cf03e8be206
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-02 22:32:17 +0100

    refactor: Update contact information in RegisterAlert component for improved user assistance

--- components/shared/registerAlert.tsx
+++ components/shared/registerAlert.tsx
@@ -29,7 +29,7 @@ export default async function RegisterAlert() {
           </CardDescription>
           <CardDescription className='text-card-foreground'>
             Si necesitas ayuda, no dudes en contactar con el coordinador de
-            sistemas.
+            sistemas <a href='https://wa.me/51941952314' target='_blank' rel='noopener noreferrer' className='text-yellow-600 hover:text-yellow-700 hover:underline'>Irvin Alvarado</a>.
           </CardDescription>
         </CardHeader>
         <CardFooter>
