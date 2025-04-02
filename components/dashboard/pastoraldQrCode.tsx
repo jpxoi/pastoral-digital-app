c9d03c4cb3f1dba863adaf49952998f593da2002
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-02 22:25:39 +0100

    refactor: Update QR code availability message and adjust color in PastoralIdQRCode component for improved clarity

--- components/dashboard/pastoraldQrCode.tsx
+++ components/dashboard/pastoraldQrCode.tsx
@@ -9,7 +9,7 @@ export default async function PastoralIdQRCode() {
   if (!isOnboarded) {
     return (
       <div className='flex aspect-square flex-col items-center justify-center rounded-lg bg-blue-50 p-4 text-center'>
-        <p className='text-xl text-blue-950'>QR Code no disponible.</p>
+        <h3 className='text-xl text-blue-950'>Código QR no disponible</h3>
         <p className='text-sm text-blue-950'>
           Por favor, completa tu registro para acceder a esta funcionalidad.
         </p>
@@ -19,7 +19,7 @@ export default async function PastoralIdQRCode() {
 
   return (
     <Image
-      src={`https://quickchart.io/qr?text=${user?.id}&size=300&margin=2&dark=001944&ecLevel=H&format=svg`}
+      src={`https://quickchart.io/qr?text=${user?.id}&size=300&margin=2&dark=172554&ecLevel=H&format=svg`}
       className='m-0 mx-auto h-auto w-full rounded-lg p-0 sm:max-w-sm'
       width={300}
       height={300}
