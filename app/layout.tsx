6b302cd60a4e5f8fb211287de8b4bd4a259ea2c9
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-08 13:52:06 +0100

    refactor: Remove Analytics and SpeedInsights components for cleaner layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -129,8 +129,6 @@ export default function RootLayout({
             richColors
           />
           <PostHogProvider>{children}</PostHogProvider>
-          <Analytics />
-          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
