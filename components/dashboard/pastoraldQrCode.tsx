cf6d1bc01d90c604c4b124215cb05e8077fd1006
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-13 07:50:05 +0100

    fix: Add error correction level to QR code URL on L, to reduce QR size

--- components/dashboard/pastoraldQrCode.tsx
+++ components/dashboard/pastoraldQrCode.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function PastoralIdQRCode({ userId }: { userId: string }) {
   return (
     <Image
-      src={`https://quickchart.io/qr?text=${userId}&size=300&margin=2&dark=172554&format=svg`}
+      src={`https://quickchart.io/qr?text=${userId}&size=300&margin=2&dark=172554&ecLevel=L&format=svg`}
       className='m-0 mx-auto h-auto w-full rounded-lg p-0 sm:max-w-sm'
       width={300}
       height={300}
