1187ca879baa47aa4251603fa3da6b99fa1da6f8
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-13 13:28:41 +0100

    fix: Improve error messages for database handling in error.ts

--- lib/error.ts
+++ lib/error.ts
@@ -2,18 +2,18 @@ import { NeonDbError } from '@neondatabase/serverless'
 
 export const handleDbError = (error: NeonDbError) => {
   if (error.message.includes('unq_user_event')) {
-    return 'Ya se registró la asistencia de este catequista para el evento actual'
+    return 'Este catequista ya tiene asistencia registrada para el evento actual. No se permiten registros duplicados.'
   } else if (error.message.includes('attendance_records_user_id_users_id_fk')) {
-    return 'No se encontró al catequista en la base de datos. Por favor, verifica que esté registrado correctamente'
+    return 'No se encontró al catequista en la base de datos. Por favor, verifica que esté registrado correctamente.'
   } else if (
     error.message.includes('attendance_records_event_id_events_id_fk')
   ) {
-    return 'No se encontró el evento en la base de datos. Por favor, verifica que haya un evento programado para hoy'
+    return 'No se encontró el evento en la base de datos. Por favor, verifica que haya un evento programado para hoy.'
   } else if (
     error.message.includes('attendance_records_registered_by_users_id_fk')
   ) {
-    return 'Tu usuario no tiene permisos para registrar asistencias. Por favor, contacta al administrador'
+    return 'Tu usuario no tiene permisos para registrar asistencias. Por favor, contacta al administrador.'
   } else {
-    return 'Ocurrió un error inesperado al registrar la asistencia. Por favor, inténtalo nuevamente'
+    return 'Ocurrió un error inesperado al registrar la asistencia. Por favor, inténtalo nuevamente.'
   }
 }
