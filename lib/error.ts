21d488ed4754db352c583e96c5822ef0f448764e
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-14 18:22:26 +0100

    fix: Improve error handling messages for attendance and mass verification

--- lib/error.ts
+++ lib/error.ts
@@ -3,7 +3,10 @@ import { NeonDbError } from '@neondatabase/serverless'
 export const handleDbError = (error: NeonDbError) => {
   if (error.message.includes('unq_user_event')) {
     return 'Este catequista ya tiene asistencia registrada para el evento actual. No se permiten registros duplicados.'
-  } else if (error.message.includes('attendance_records_user_id_users_id_fk')) {
+  } else if (
+    error.message.includes('attendance_records_user_id_users_id_fk') ||
+    error.message.includes('sunday_masses_user_id_users_id_fk')
+  ) {
     return 'No se encontró al catequista en la base de datos. Por favor, verifica que esté registrado correctamente.'
   } else if (
     error.message.includes('attendance_records_event_id_events_id_fk')
@@ -15,7 +18,9 @@ export const handleDbError = (error: NeonDbError) => {
     return 'Tu usuario no tiene permisos para registrar asistencias. Por favor, contacta al administrador.'
   } else if (error.message.includes('unq_user_sunday_date')) {
     return 'Ya has registrado una misa para hoy. No se permiten registros duplicados.'
+  } else if (error.message.includes('sunday_masses_verified_by_users_id_fk')) {
+    return 'No tienes permisos para verificar asistencia a misa. Por favor, contacta al administrador.'
   } else {
-    return 'Ocurrió un error inesperado al enviar la solicitud. Por favor, intenta nuevamente más tarde.'
+    return 'Ha ocurrido un error inesperado. Por favor, intenta nuevamente más tarde.'
   }
 }
