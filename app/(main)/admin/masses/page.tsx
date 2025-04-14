67a4a025d67310a86889659efee5410cbf91bc10
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-14 18:25:42 +0100

    feat: Create page component for managing masses with dynamic rendering

---
+++ app/(main)/admin/masses/page.tsx
@@ -0,0 +1,33 @@
+import { Suspense } from 'react'
+import { Metadata } from 'next'
+import OfflineAlert from '@/components/shared/offlineAlert'
+import UsersTable from '@/components/admin/usersTable'
+import UserTableSkeleton from '@/components/admin/userTableSkeleton'
+import MassesTable from '@/components/admin/massesTable'
+
+export const metadata: Metadata = {
+  title: 'Administrar Misas | Pastoral Digital App',
+}
+
+export default function Page() {
+  return (
+    <main className='flex h-full w-full flex-1 flex-col gap-4 overflow-y-scroll rounded-tl-2xl border border-neutral-200 bg-white p-4 pb-8 max-sm:max-h-[calc(100vh-3rem)] md:p-8'>
+      <Suspense fallback={null}>
+        <OfflineAlert />
+      </Suspense>
+      <div className='flex flex-col gap-2 text-left'>
+        <h1 className='text-xl font-semibold sm:text-2xl'>
+          Administrar Misas
+        </h1>
+        <p className='text-sm text-neutral-500'>
+          Esta es la lista de misas registradas en la aplicación.
+        </p>
+      </div>
+      <Suspense fallback={<UserTableSkeleton />}>
+        <MassesTable />
+      </Suspense>
+    </main>
+  )
+}
+
+export const dynamic = 'force-dynamic'
