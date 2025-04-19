220283cb56946a2920315c7691995539402f8c8b
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-19 16:24:34 +0100

    fix: Swap labels for student and alumni in UserCategoryLabel component

--- components/shared/userCategoryLabel.tsx
+++ components/shared/userCategoryLabel.tsx
@@ -9,16 +9,16 @@ export default function UserCategoryLabel({
   category: UserCategory
 }) {
   const categoryMapping: { [key: string]: JSX.Element } = {
-    alumni: (
-      <Badge variant={'blue'} className='h-8 rounded-lg'>
-        <IconBackpack className='size-4' />
-        <span className='ml-2 text-nowrap'>Exalumno</span>
-      </Badge>
-    ),
     student: (
       <Badge variant={'emerald'} className='h-8 rounded-lg'>
-        <IconSchoolOff className='size-4' />
+        <IconBackpack className='size-4' />
         <span className='ml-2 text-nowrap'>Alumno</span>
+      </Badge>
+    ),
+    alumni: (
+      <Badge variant={'blue'} className='h-8 rounded-lg'>
+        <IconSchoolOff className='size-4' />
+        <span className='ml-2 text-nowrap'>Exalumno</span>
       </Badge>
     ),
     teacher: (
