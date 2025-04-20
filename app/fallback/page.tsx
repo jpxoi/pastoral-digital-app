ddb1a84b661cc675e8a5110d0db057f6de32f4a3
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-20 13:56:16 +0100

    refactor: Revamp OfflinePage layout and content for improved user experience

--- app/fallback/page.tsx
+++ app/fallback/page.tsx
@@ -1,29 +1,27 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { IconWifiOff } from '@tabler/icons-react'
 
 export default function OfflinePage() {
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center gap-8 px-4'>
-      <div className='w-52 md:w-96'>
-        <Image
-          src='/graphics/offline.svg'
-          alt='Offline'
-          unoptimized={true}
-          width={384}
-          height={208}
-        />
-      </div>
-      <div className='flex flex-col items-center justify-center gap-2'>
-        <h1 className='text-balance text-2xl font-bold md:text-3xl'>
-          ¡Oops! Parece que no tienes conexión a internet
+    // Use a neutral background
+    <div className='flex min-h-screen w-full flex-col items-center justify-center bg-primary p-4'>
+      <div className='flex max-w-md flex-col items-center justify-center gap-6 rounded-lg bg-background p-10 text-center shadow-lg transition-shadow duration-300'>
+        <IconWifiOff className='h-20 w-20 text-destructive' stroke={2} />
+        <h1 className='mt-4 text-balance text-xl font-extrabold tracking-tight md:text-2xl'>
+          ¡Oops! Sin conexión
         </h1>
-        <p className='text-md md:text-lg'>
-          Por favor, revisa tu conexión y vuelve a intentarlo.
+        <p className='text-muted-foreground'>
+          Parece que no estás conectado a internet. Revisa tu conexión y vuelve
+          a intentarlo.
         </p>
-        <Button onClick={() => location.reload()} className=''>
-          Volver a intentar
+        <Button
+          onClick={() => location.reload()}
+          // Larger, rounded, more padding, hover effect
+          size='lg'
+        >
+          Reintentar
         </Button>
       </div>
     </div>
