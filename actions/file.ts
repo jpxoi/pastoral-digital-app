f2acee17898e021239464967b16f14883bfe9517
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-21 07:59:54 +0100

    refactor: Remove unused deleteFiles function from file.ts

--- actions/file.ts
+++ actions/file.ts
@@ -5,7 +5,3 @@ import { utapi } from '@/server/uploadthing'
 export async function deleteFile(key: string) {
   await utapi.deleteFiles(key)
 }
-
-export async function deleteFiles(keys: string[]) {
-  await utapi.deleteFiles(keys)
-}
