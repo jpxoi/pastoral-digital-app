1a72ff10e0cbc3b7d12572100614966ef56bb966
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-21 11:55:37 +0100

    refactor: replace utapi file deletion with Uploadcare API integration

--- actions/file.ts
+++ actions/file.ts
@@ -1,7 +1,57 @@
 'use server'
 
-import { utapi } from '@/server/uploadthing'
+import { deleteFile, UploadcareSimpleAuthSchema } from '@uploadcare/rest-client'
+import { generateSecureSignature } from '@uploadcare/signed-uploads'
 
-export async function deleteFile(key: string) {
-  await utapi.deleteFiles(key)
+const uploadcareSimpleAuthSchema = new UploadcareSimpleAuthSchema({
+  publicKey: process.env.NEXT_PUBLIC_UPLOADCARE_PUBLIC_KEY as string,
+  secretKey: process.env.UPLOADCARE_SECRET_KEY as string,
+})
+
+export async function removeFile(uuid: string): Promise<{
+  success?: boolean
+  error?: string
+}> {
+  return await deleteFile(
+    {
+      uuid: uuid,
+    },
+    { authSchema: uploadcareSimpleAuthSchema }
+  )
+    .then((file) => {
+      console.log('File deleted successfully:', file.uuid)
+      return {
+        success: true,
+      }
+    })
+    .catch((error) => {
+      console.error('Error deleting file:', error.message)
+
+      if (error.message == '[404 Not Found] Not found.') {
+        return {
+          success: true,
+        }
+      }
+
+      return {
+        error: error.message,
+      }
+    })
+}
+
+export async function getUploadcareSignature(): Promise<{
+  secureSignature: string
+  secureExpire: string
+}> {
+  const { secureSignature, secureExpire } = generateSecureSignature(
+    process.env.UPLOADCARE_SECRET_KEY as string,
+    {
+      lifetime: 60 * 10 * 1000, // expire in 10 minutes
+    }
+  )
+
+  return {
+    secureSignature,
+    secureExpire,
+  }
 }
