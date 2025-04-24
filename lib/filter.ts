9130a03b08dcf403054cce1235a31992593f9015
Jean Paul Fernandez <jpxoi@icloud.com>
2025-04-24 22:53:18 +0100

    feat: add filter options for user categories, schedules, and attendance statuses

---
+++ lib/filter.ts
@@ -0,0 +1,97 @@
+import { AttendanceStatus, UserCategory, UserSchedule } from '@/types'
+
+import {
+  IconBackpack,
+  IconCalendarBolt,
+  IconCalendarHeart,
+  IconCalendarX,
+  IconClockCheck,
+  IconClockExclamation,
+  IconClockHeart,
+  IconClockX,
+  IconCross,
+  IconFlame,
+  IconMoodKid,
+  IconPackageExport,
+  IconSchool,
+  IconSchoolOff,
+} from '@tabler/icons-react'
+
+export const UserCategoryFilterOptions = [
+  {
+    label: 'Alumno',
+    value: UserCategory.STUDENT,
+    icon: IconBackpack,
+  },
+  {
+    label: 'Exalumno',
+    value: UserCategory.ALUMNI,
+    icon: IconSchoolOff,
+  },
+  {
+    label: 'Docente',
+    value: UserCategory.TEACHER,
+    icon: IconSchool,
+  },
+]
+
+export const UserScheduleFilterOptions = [
+  {
+    label: 'Tiempo Completo',
+    value: UserSchedule.FULL_TIME,
+    icon: IconCalendarBolt,
+  },
+  {
+    label: 'Primera Comunión',
+    value: UserSchedule.PRIMERA_COMUNION,
+    icon: IconCross,
+  },
+  {
+    label: 'Confirmación',
+    value: UserSchedule.CONFIRMACION,
+    icon: IconFlame,
+  },
+  {
+    label: 'Logística',
+    value: UserSchedule.LOGISTICA,
+    icon: IconPackageExport,
+  },
+  {
+    label: 'Semilleros',
+    value: UserSchedule.SEMILLEROS,
+    icon: IconMoodKid,
+  },
+]
+
+export const AttendanceStatusFilterOptions = [
+  {
+    label: 'A TIEMPO',
+    value: AttendanceStatus.A_TIEMPO,
+    icon: IconClockCheck,
+  },
+  {
+    label: 'TARDANZA',
+    value: AttendanceStatus.TARDANZA,
+    icon: IconClockExclamation,
+  },
+  {
+    label: 'DOBLE TARDANZA',
+    value: AttendanceStatus.DOBLE_TARDANZA,
+    icon: IconClockX,
+  },
+  {
+    label: 'FALTA JUSTIFICADA',
+    value: AttendanceStatus.FALTA_JUSTIFICADA,
+    icon: IconCalendarHeart,
+  },
+  {
+    label: 'TARDANZA JUSTIFICADA',
+    value: AttendanceStatus.TARDANZA_JUSTIFICADA,
+    icon: IconClockHeart,
+  },
+  {
+    label: 'FALTA NO JUSTIFICADA',
+    value: AttendanceStatus.FALTA_INJUSTIFICADA,
+    icon: IconCalendarX,
+  },
+]
