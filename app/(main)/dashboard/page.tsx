5d9ad39e711383125abddfe0b9cb3070294ed9b0
Jean Paul Fernandez <jpxoi@icloud.com>
2026-02-18 13:53:24 +0000

    Remove onboarding alert and show ID

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import AttendanceSection from '@/components/dashboard/attendanceSection'
 import OfflineAlert from '@/components/shared/offlineAlert'
-import RegisterAlert from '@/components/shared/registerAlert'
 import { Metadata } from 'next'
 import { Suspense } from 'react'
 
@@ -16,9 +15,6 @@ export default function PastoralDigitalPage() {
     <main className='flex h-full w-full flex-1 flex-col gap-4 overflow-y-scroll rounded-tl-2xl border border-neutral-200 bg-white p-4 pb-8 max-sm:max-h-[calc(100vh-3rem)] md:p-8'>
       <Suspense fallback={null}>
         <OfflineAlert />
-      </Suspense>
-      <Suspense fallback={null}>
-        <RegisterAlert />
       </Suspense>
 
       {/* Sección de Pastoral ID y Asistencia */}
