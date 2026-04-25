d416d2c2cf4409feb3dcc2cf40577910c523f833
Jean Paul Fernandez <jpxoi@icloud.com>
2026-04-25 08:48:31 +0100

    refactor: remove RedirectToDashboard component and implement redirect logic directly in Home component
    This commit eliminates the RedirectToDashboard component and integrates its functionality directly into the Home component. The Home component now checks for user authentication and redirects to the dashboard if a user is logged in, streamlining the code and improving performance.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Background from '@/components/shared/background'
 import WelcomeScreen from '@/components/home/welcomeScreen'
-import RedirectToDashboard from '@/components/shared/redirectToDashboard'
-import { Suspense } from 'react'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+  if (userId) redirect('/dashboard')
+
   return (
     <main className='flex h-screen w-full flex-col items-center justify-center bg-center'>
       <Background />
       <WelcomeScreen />
-      <Suspense fallback={null}>
-        <RedirectToDashboard />
-      </Suspense>
     </main>
   )
 }
--- components/shared/redirectToDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
-
-export default async function RedirectToDashboard() {
-  const { userId } = await auth()
-  if (userId) redirect('/dashboard')
-
-  return null
-}
